0d9c628c4e994c8961b1e21f074881c5eff167f4
Ange Chierchia <achierchia@conceptfactory.lu>
2024-03-05 23:05:22 +0100

    Add entry-footer template, customize post meta display, and adjust styles. Add function to modify post formats support.

--- functions.php
+++ functions.php
@@ -53,3 +53,24 @@ function actheme_remove_parent_post_formats() {
     remove_post_type_support('post', 'post-formats');
 }
 add_action('init', 'actheme_remove_parent_post_formats', 11);
+
+if ( ! function_exists( 'sempress_posted_on' ) ) :
+	/**
+	 * Prints HTML with meta information for the current post-date/time and author.
+	 * Create your own sempress_posted_on to override in a child theme
+	 *
+	 * @since SemPress 1.0.0
+	 */
+	function sempress_posted_on() {
+		printf( __( '<span class="sep">Publié le </span><a href="%1$s" title="%2$s" rel="bookmark" class="url u-url"><time class="entry-date updated published dt-updated dt-published" datetime="%3$s" itemprop="dateModified datePublished">%4$s</time></a><address class="byline"> <span class="sep"> par </span> <span class="author p-author vcard hcard h-card" itemprop="author " itemscope itemtype="http://schema.org/Person">%5$s <a class="url uid u-url u-uid fn p-name" href="%6$s" title="%7$s" rel="author" itemprop="url"><span itemprop="name">%8$s</span></a></span></address>', 'sempress' ),
+			esc_url( get_permalink() ),
+			esc_attr( get_the_time() ),
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			get_avatar( get_the_author_meta( 'ID' ), 90 ),
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_attr( sprintf( __( 'View all posts by %s', 'sempress' ), get_the_author() ) ),
+			esc_html( get_the_author() )
+		);
+	}
+endif;
