acb30e40e88a80dc03dfab7510024c556fbaf71d
Ange Chierchia <achierchia@conceptfactory.lu>
2024-03-17 15:14:49 +0100

    add custom layout for reply posts

---
+++ kind_views/kind-reply.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Reply Template
+ *
+ */
+
+ if ( ! $cite ) {
+	return;
+}
+
+?>
+
+
+<section class="response <?php echo empty( $url ) ? 'p-in-reply-to' : 'u-in-reply-to'; ?> h-cite">
+<header>
+<?php
+echo Kind_Taxonomy::get_before_kind( 'reply' );
+if ( ! $embed ) {
+	if ( ! empty( $cite['url'] ) ) {
+		echo sprintf( '<a href="%1s" class="p-name u-url">%2s</a>', $cite['url'], $cite['name'] );
+	} else {
+		echo sprintf( '<span class="p-name">%1s</span>', esc_html( $cite['name'] ) );
+	}
+	if ( $author ) {
+		echo ' ' . __( 'by', 'indieweb-post-kinds' ) . ' ' . $author;
+	}
+	if ( ! empty( $cite['publication'] ) ) {
+		echo sprintf( ' <em>(<span class="p-publication">%1s</span>)</em>', esc_html( $cite['publication'] ) );
+	}
+}
+?>
+</header>
+<?php
+if ( ! empty( $embed ) ) {
+	echo sprintf( '<blockquote class="e-summary">%1s</blockquote>', $embed );
+} elseif ( ! empty( $cite['summary'] ) ) {
+	echo sprintf( '<blockquote class="e-summary">%1s</blockquote>', $cite['summary'] );
+}
+
+// Close Response
+?>
+</section>
+
+<?php
